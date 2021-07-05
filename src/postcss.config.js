9509232a1e57f3b567d7758103de2c7b4c4117f0
Aravind Reddy V <zerodividedby0@gmail.com>
2021-07-05 15:37:01 +0530

    Update tailwind jit

--- src/postcss.config.js
+++ src/postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    require(`@tailwindcss/jit`)(`./src/tailwind/tailwind.config.js`),
+    require(`tailwindcss`)(`./src/tailwind/tailwind.config.js`),
     require(`autoprefixer`),
   ],
 };
